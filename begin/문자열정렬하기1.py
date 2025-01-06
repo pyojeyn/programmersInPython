365a2c0bde82d920cb8dba8450847e7113a3ca28
pyojeyn <jeynpyo@gmail.com>
2025-01-06 12:24:36 +0900

    문자열정렬하기1

---
+++ begin/문자열정렬하기1.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    answer = []
+
+    for s in my_string:
+        if s.isdigit():
+            answer.append(int(s))
+
+    answer.sort()        
+    return answer
+
+print(solution('hi12392'))
